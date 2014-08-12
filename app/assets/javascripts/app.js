97a55af239b737e97d916ae6a455200b438afe54
Michael Sofaer <github@mike.sofaer.net>
2014-08-11 18:25:03 -0700

    Some templating stuff

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,22 @@
+var HomePage = Backbone.View.extend({
+  render: function(){
+    var template = $("#home-template")
+    var compiled = _.template(template.html())
+    var innerTemplate = current_user ? UserBoxView : SignUpView
+    this.$el.html(compiled({user: current_user}))
+    new innerTemplate({el: this.$(".user_box")}).render()
+  }
+})
+
+var SignUpView = Backbone.View.extend({
+  render: function(){
+    var template = $("#sign-up-template")
+    var compiled = _.template(template.html())
+    this.$el.html(compiled())
+  }
+})
+
+
 var MainRouter = Backbone.Router.extend({
 
   routes: {
@@ -5,10 +24,13 @@ var MainRouter = Backbone.Router.extend({
   },
 
   home: function() {
-    console.log("hi")
+    var el = $("#main")[0]
+    new HomePage({el: el}).render()
   }
 
 })
 
 new MainRouter()
+$(function(){
 console.log(Backbone.history.start({pushState: true}))
+})
