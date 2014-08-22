ecd01db228fd85a91ff7ad858e648a260a81a63f
Michael Sofaer <github@mike.sofaer.net>
2014-08-21 19:16:05 -0700

    respond to payments call

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,7 @@ class PaymentsController < ApplicationController
   def index #Tell Blockchain about it
     user = User.where(funding_address: params[:address], funding_secret: params[:secret]).first
     if user
-      Payment.create(
+      payment = Payment.create(
         user_id: user.id,
         address: params[:address],
         value: params[:value],
@@ -11,6 +11,11 @@ class PaymentsController < ApplicationController
         input_transaction_hash: params[:input_transaction_hash],
         transaction_hash: params[:transaction_hash],
       )
+      if payment.persisted?
+        render text: "ok"
+        return
+      end
     end
+    render text: "not ok"
   end
 end
