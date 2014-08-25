0c4f58d1950a5ff96f3c8967461600db8ab50358
Michael Sofaer <github@mike.sofaer.net>
2014-08-25 14:39:09 -0700

    Save all payment attempts

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,23 +1,25 @@
 class PaymentsController < ApplicationController
   def index #Tell Blockchain about it
     pa = PaymentAddress.where(address: params[:input_address], secret: params[:secret]).first
-    if pa
-      payment = Payment.create(
-        payment_address: pa,
-        address: params[:address],
-        value: params[:value],
-        destination_address: params[:destination_address],
-        input_address: params[:input_address],
-        input_transaction_hash: params[:input_transaction_hash],
-        transaction_hash: params[:transaction_hash],
-      )
-      if payment.persisted?
-        render text: "*ok*"
+    payment = Payment.create(
+      payment_address: pa,
+      address: params[:address],
+      value: params[:value],
+      destination_address: params[:destination_address],
+      input_address: params[:input_address],
+      input_transaction_hash: params[:input_transaction_hash],
+      transaction_hash: params[:transaction_hash],
+    )
+    if payment.persisted?
+      render text: "*ok*"
+      if pa
         payment.process!
         pa.user.ensure_stellar_wallet.issue(payment.value)
         UserMailer.payment_email(pa.user, payment).deliver
-        return
+      else
+        puts "Bad payment received"
       end
+      return
     end
     render text: "not ok"
   end
